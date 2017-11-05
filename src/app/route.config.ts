0f38db09d08d7d3ab8b69d88c9e479e61df841b8
olgmaks <drolgmaks@gmail.com>
2017-11-05 13:19:58 +0200

    Added AppRoutingModule instead of export json

--- src/app/route.config.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {RecipesComponent} from './recipes/recipes.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-
-export const appRoutes: Routes = [
-  {path: '', component: RecipesComponent},
-  {path: 'recipes', component: RecipesComponent},
-  {path: 'shopping-list', component: ShoppingListComponent}
-];
